4529711791c481a7530177367318dfd1b7714bdf
vmdev01 <peter@ubuntu.(none)>
2012-09-09 22:50:33 -0700

    new script

---
+++ deploy_setup.rb
@@ -0,0 +1,14 @@
+#!/usr/bin/env ruby
+
+`rvmsudo gem install sinatra`
+
+`eval \`ssh-agent\``
+
+`ssh-add`
+
+`nohup ruby -rubygems ./auto_deploy.rb  -p 10304 &`
+
+`nohup rake ts:dd &`
+
+`sudo /etc/init.d/nginx stop`
+`sudo /etc/init.d/nginx start`
